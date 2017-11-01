ffa4d1a3c1b8471c1a027b96abe2c7dd93f0f6ca
Dipack P Panjabi <dipack95@gmail.com>
2017-11-01 13:10:01 +0530

    Second try

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::fmt::{self, Formatter, Display};
+
+struct City {
+    name: &'static str,
+    latitude: f32,
+    longitude: f32
+}
+
+impl Display for City {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let latitude_direction = if self.latitude >= 0.0 { "North" } else { "South" };
+        let longitude_direction = if self.longitude >= 0.0 { "East" } else { "West" };
+
+        write!(f, 
+            "{}: {:.3}deg{} {:.3}deg{}", 
+            self.name, self.latitude.abs(), latitude_direction,
+            self.longitude.abs(), longitude_direction
+        )
+    }
+}
+
 fn main() {
-    println!("Hello world!")
+    for city in [
+        City {name: "Dublin", latitude: 44.67, longitude: -7.56},
+        City {name: "Mumbai", latitude: 12.34, longitude: 12.74}
+    ].iter() {
+        println!("{}", *city);
+    }
 }
