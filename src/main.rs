3d550ddf6bbac3ba1f49c87759fd948972caab95
Dipack P Panjabi <dipack95@gmail.com>
2018-02-03 23:07:43 +0530

    Refactor method to return array of all accessible objects and a loop to display them

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate walkdir;
 
 use notify::{RecommendedWatcher, Watcher, RecursiveMode};
 use walkdir::WalkDir;
+use std::collections::LinkedList;
 
 fn watch(watch_directory: &str) -> notify::Result<()> {
     let (tx, rx) = std::sync::mpsc::channel();
@@ -19,16 +20,32 @@ fn watch(watch_directory: &str) -> notify::Result<()> {
     }
 }
 
-fn get_accessible_sub_directories(directory: &str) {
-    for dir in WalkDir::new(directory).into_iter().filter_map(|d| d.ok()) {
-        println!("{}", dir.path().display());
+fn get_accessible_objects(directory: &str, max_depth: Option<u32>) -> LinkedList<String> {
+    let mut accessible_entries = LinkedList::new();
+    for dir in WalkDir::new(directory).max_depth(max_depth.unwrap_or(1) as usize).into_iter().filter_map(|d| d.ok()) {
+        accessible_entries.push_back(dir.path().display().to_string());
     }
+    accessible_entries
 }
 
 fn main() {
-    let watch_directory = "D:\\Scripts";
+    let root_directory: &str = "D:\\";
+    let watch_directory: &str = "D:\\Scripts";
 
-//    get_accessible_sub_directories(watch_directory);
+    let entries: LinkedList<String> = get_accessible_objects(root_directory, Some(2));
+
+    println!("Number of all accessible objects under {} is {:?}", root_directory, entries.len());
+
+    let mut iter = entries.iter();
+    loop {
+        match iter.next() {
+            Some(ob) => { println!("Object: {:?}", ob) }
+            _ => {
+                println!("Nothing found!");
+                break;
+            }
+        }
+    }
 
     if let Err(event) = watch(watch_directory) {
         println!("Main thread: Error: {:?}", event)
